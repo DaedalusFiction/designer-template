41cab67a39396ce9773a42169e588a07fce24c5f
djs41286 <teardownthismall@gmail.com>
2022-09-06 17:29:40 -0400

    added hover effect

--- pages/projects/index.js
+++ pages/projects/index.js
@@ -6,6 +6,7 @@ import PageLayout from "../../components/layout/PageLayout";
 import { galleryConfigs } from "../../siteInfo";
 import SizedImage from "../../components/general/SizedImage";
 import Link from "next/link";
+import lightTheme from "../../styles/themes/lightTheme";
 
 const Projects = () => {
   return (
@@ -15,12 +16,35 @@ const Projects = () => {
           return (
             <Grid key={index} item xs={12} sm={6} md={4}>
               <Link href={galleryConfig.href}>
-                <Box>
-                  <SizedImage
-                    image={galleryConfig.image}
-                    height="25rem"
-                    width="100%"
-                  />
+                <Box sx={{ position: "relative" }}>
+                  <Box
+                    className="link"
+                    sx={{
+                      transition: "300ms",
+                      "&:hover": { filter: "brightness(60%)" },
+                    }}
+                  >
+                    <SizedImage
+                      image={galleryConfig.image}
+                      height="25rem"
+                      width="100%"
+                    />
+                  </Box>
+                  <Typography
+                    variant="h4"
+                    sx={{
+                      position: "absolute",
+                      textTransform: "uppercase",
+                      fontWeight: "600",
+                      top: "50%",
+                      left: "0",
+                      textAlign: "center",
+                      width: "100%",
+                      color: lightTheme.palette.custom.light,
+                    }}
+                  >
+                    {galleryConfig.category}
+                  </Typography>
                 </Box>
               </Link>
             </Grid>
