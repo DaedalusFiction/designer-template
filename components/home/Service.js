04edb63b1e1a72aeb3cd6f9406b19f68b65b391b
David Sorensen <teardownthismall@gmail.com>
2022-09-10 13:19:17 -0400

    set up services page, contact form/page

--- components/home/Service.js
+++ components/home/Service.js
@@ -1,89 +1,91 @@
 import { Check } from "@mui/icons-material";
 import {
-  Button,
-  List,
-  ListItem,
-  ListItemIcon,
-  ListItemText,
-  Paper,
-  Typography,
+    Button,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText,
+    Paper,
+    Typography,
 } from "@mui/material";
 import { Box } from "@mui/system";
 import Link from "next/link";
 import SizedImage from "../general/SizedImage";
 
 const Service = ({ service, preview }) => {
-  return (
-    <Paper
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "space-between",
-        transition: "300ms",
-        height: "100%",
-        "&:hover": {
-          boxShadow: "5px 5px 5px rgba(0, 0, 0, 0.3)",
-          transform: "translate(-3px, -3px)",
-        },
-      }}
-    >
-      <Box>
-        <SizedImage height="12rem" width="100%" image={service.image} />
-        <Box sx={{ padding: "2rem 1rem", flexGrow: "1" }}>
-          <Typography
-            sx={{ textAlign: "center", fontWeight: "400" }}
-            variant="h3"
-          >
-            {service.title}
-          </Typography>
-          <br />
-          <Typography variant="h4" sx={{ textAlign: "center" }}>
-            {service.price}
-          </Typography>
-          <br />
-          <Typography sx={{ textAlign: "center" }}>
-            {service.description}
-          </Typography>
-          <br />
-          <br />
-          {preview && (
-            <Box>
-              <List dense>
-                {service.features.map((feature, index) => {
-                  return (
-                    <ListItem key={index}>
-                      <ListItemIcon>
-                        <Check />
-                      </ListItemIcon>
-                      <ListItemText
-                        primary={feature.primaryText}
-                        secondary={feature.secondaryText}
-                      />
-                    </ListItem>
-                  );
-                })}
-              </List>
-            </Box>
-          )}
-        </Box>
-        </Box>
-        <Box
-          className="flex-1"
-          sx={{ justifyContent: "center", marginBottom: "1em"}}
+    return (
+        <Paper
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                transition: "300ms",
+                height: "100%",
+                "&:hover": {
+                    boxShadow: "5px 5px 5px rgba(0, 0, 0, 0.3)",
+                    transform: "translate(-3px, -3px)",
+                },
+            }}
         >
-          <Link href="/about">
-            <Button
-              variant="contained"
-              color="secondary"
-              size="large"
-              // tabIndex={-1}
+            <Box>
+                <SizedImage height="12rem" width="100%" image={service.image} />
+                <Box sx={{ padding: "2rem 1rem" }}>
+                    <Typography
+                        sx={{ textAlign: "center", fontWeight: "400" }}
+                        variant="h3"
+                    >
+                        {service.title}
+                    </Typography>
+                    <br />
+                    <Typography variant="h4" sx={{ textAlign: "center" }}>
+                        {service.price}
+                    </Typography>
+                    <br />
+                    <Typography sx={{ textAlign: "center" }}>
+                        {service.description}
+                    </Typography>
+                    <br />
+                    <br />
+                    {!preview && (
+                        <Box>
+                            <List dense>
+                                {service.features.map((feature, index) => {
+                                    return (
+                                        <ListItem key={index}>
+                                            <ListItemIcon>
+                                                <Check />
+                                            </ListItemIcon>
+                                            <ListItemText
+                                                primary={feature.primaryText}
+                                                secondary={
+                                                    feature.secondaryText
+                                                }
+                                            />
+                                        </ListItem>
+                                    );
+                                })}
+                            </List>
+                        </Box>
+                    )}
+                </Box>
+            </Box>
+            <Box
+                className="flex-1"
+                sx={{ justifyContent: "center", marginBottom: "1em" }}
             >
-              {service.button.text}
-            </Button>
-          </Link>
-        </Box>
-    </Paper>
-  );
+                <Link href={service.button.href}>
+                    <Button
+                        variant="contained"
+                        color="secondary"
+                        size="large"
+                        // tabIndex={-1}
+                    >
+                        {service.button.text}
+                    </Button>
+                </Link>
+            </Box>
+        </Paper>
+    );
 };
 
 export default Service;
